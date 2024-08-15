6a6aeedd4641fc0af8108a6e85719749159db124
Bruno Eiji Mine <brunomine151@gmail.com>
2024-08-14 22:51:48 -0300

    Ajustando favicon e adicionando funçao para o navMobile

--- src/components/Header/NavMobile/navMobile.js
+++ src/components/Header/NavMobile/navMobile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { 
     NavMobileContainer,
     NavMobileButton, 
@@ -6,19 +6,18 @@ import {
     NavMobileTitle, 
     MobileListWrapper,
     MobileListContainer 
-    } 
-from "./navMobileStyles";
+} from "./navMobileStyles";
 
 // Logo
-import logo from "../../../assets/img/logo-kombini.png"
+import logo from "../../../assets/img/logo-kombini.png";
 
-// Biblioteca de icones
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faX } from '@fortawesome/free-solid-svg-icons'
+// Biblioteca de ícones
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const links = [
     {
-        name: "Inicio",
+        name: "Início",
         href: "/",
     },
     {
@@ -33,40 +32,63 @@ const links = [
         name: "Contato",
         href: "/contato",
     },
-]
-
+];
 
 const NavMobile = () => {
 
+    // Estados para fechar o nav
+    const [navOpen, setNavOpen] = useState(false)
+
+    const openNav = () => {
+        setNavOpen(true)
+    }
+
+    const closeNav = () => {
+        setNavOpen(false)
+    }
+
+    // Aqui realizo a funçao para abrir o nav
+    const [isVisible, setIsVisible] = useState(window.innerWidth < 1200);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsVisible(window.innerWidth < 1200);
+            openNav();
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     return (
         <>
-            
-            <BackgroundNavOpacity />{/* Opacidade do fundo ao ativar o mobile */}
+            {isVisible && navOpen && (
+                <>
+                    <BackgroundNavOpacity /> {/* Opacidade do fundo ao ativar o mobile */}
+                    <NavMobileContainer>
+                        <MobileListWrapper>
+                            <NavMobileTitle>
+                                <img src={logo} alt="Logo Kombini" className="logo" />
+                                <FontAwesomeIcon className="icon" icon={faX} onClick={closeNav}/>
+                            </NavMobileTitle>
 
-            <NavMobileContainer>
-                <MobileListWrapper>
-                    <NavMobileTitle>
-                        <img src={logo} className="logo"/>
-                        <FontAwesomeIcon className="icon" icon={faX} />
-                    </NavMobileTitle>
-
-                    <MobileListContainer>
-                        {links.map((link, index) => {
-                            return (
-                                <a href={link.href} key={index}>
-                                    {link.name}
-                                </a>
-                            );
-                        })}
-                        <NavMobileButton>Nos visite</NavMobileButton>
-                    </MobileListContainer>
-                </MobileListWrapper>
-
-            </NavMobileContainer>
-
+                            <MobileListContainer>
+                                {links.map((link, index) => (
+                                    <a href={link.href} key={index}>
+                                        {link.name}
+                                    </a>
+                                ))}
+                                <NavMobileButton>Nos visite</NavMobileButton>
+                            </MobileListContainer>
+                        </MobileListWrapper>
+                    </NavMobileContainer>
+                </>
+            )}
         </>
     );
+};
 
-}
-
-export default NavMobile
+export default NavMobile;
